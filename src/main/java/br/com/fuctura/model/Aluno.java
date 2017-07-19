1a9aae29b95202778f9fbb62d517bd8ce760aa8c
Douglas Fernandes <professordouglas.filho@gmail.com>
2017-07-19 20:33:15 -0300

    atualização do pom.xml para tomcat

--- src/main/java/br/com/fuctura/model/Aluno.java
+++ src/main/java/br/com/fuctura/model/Aluno.java
@@ -1,8 +1,35 @@
 package br.com.fuctura.model;
 
+import java.util.List;
+
 public class Aluno {
 	private String nome;
 	private String turma;
 	private String serie;
+	private List<Materia> materias;
 	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTurma() {
+		return turma;
+	}
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	public String getSerie() {
+		return serie;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	public List<Materia> getMaterias() {
+		return materias;
+	}
+	public void setMaterias(List<Materia> materias) {
+		this.materias = materias;
+	}
 }
---
+++ src/main/java/br/com/fuctura/model/Materia.java
@@ -0,0 +1,19 @@
+package br.com.fuctura.model;
+
+public class Materia {
+	private String nome;
+	private double nota;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getNota() {
+		return nota;
+	}
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+}
